9987a2f3eb78c1d3bc2607000b9f2b6d6dd44008
kisnlsk <161097532+kisnlsk@users.noreply.github.com>
2024-03-26 16:39:33 +0900

    Update variables.tf

--- terraform_vcn_compute_demo/variables.tf
+++ terraform_vcn_compute_demo/variables.tf
@@ -33,6 +33,6 @@ variable "instance_shape_config_memory_in_gbs" {
   default = 1
 }
 
-variable oci_core_instance--source_details-source_id--export_test-instance-2 { default = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaay72qyswcworlh5phdr67q7aezs2ecmhbrs2yqs7x6iudtxw22qta" }
+variable oci_core_instance--source_details-source_id-2--export_test-instance-2 { default = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaay72qyswcworlh5phdr67q7aezs2ecmhbrs2yqs7x6iudtxw22qta" }
 variable availability_domain--nNMJ-AP-TOKYO-1-AD-1 { default = "nNMJ:AP-TOKYO-1-AD-1" }
 variable fault_domain--FAULT-DOMAIN-3 { default = "FAULT-DOMAIN-3" }
